73a4aa4f8bd4dc8df5d2a7da84b365f0f16aac99
Farid <farid.ousmane01@gmail.com>
2025-02-20 23:50:37 +0100

    Refactor Tweet model to use datetime.now for created_at and add migration for new column and indexes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from . import db
-import datetime
+from datetime import datetime
 
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False, index=True)
     positive = db.Column(db.Boolean, nullable=False, index=True)
     negative = db.Column(db.Boolean, nullable=False, index=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
